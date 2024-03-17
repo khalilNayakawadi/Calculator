f703ae05a62592324b2101aea0c36c619f817a3a
khalilNayakawadi <136471783+khalilNayakawadi@users.noreply.github.com>
2024-03-17 13:54:41 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let inputValue = '';
+
+function addValue(number)
+{
+    inputValue += number;
+    document.getElementById('displayVal').value = inputValue;
+    
+}
+
+function allClear(number)
+{
+    inputValue = '';
+    document.getElementById('displayVal').value = '';
+
+}
+
+function clearValue()
+{
+    inputValue =  Math.floor(inputValue/10);
+     document.getElementById('displayVal').value =inputValue;
+     if(inputValue == 0)
+     {
+        inputValue = '';
+     }
+}
+
+function displayResult(number)
+{
+    inputValue = eval(inputValue);
+    document.getElementById('displayVal').value = inputValue;
+     if (inputValue == 0 ) {
+        inputValue = '';
+     }
+}
